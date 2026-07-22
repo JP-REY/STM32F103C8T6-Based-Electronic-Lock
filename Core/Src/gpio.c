dd706b77eb4003ba106300fb446ee69efac0eb47
Joao Pedro Rey <jprey.380@hotmail.com>
2026-07-22 17:00:31 -0300

    chore(cubemx): update .ioc with board peripherals and CubeMX configuration

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -54,12 +54,12 @@ void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(LED_BOARD_GPIO_Port, LED_BOARD_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, MKB_IN1_PIN_Pin|MKB_IN2_PIN_Pin|MKB_IN3_PIN_Pin|MKB_IN4_PIN_Pin
-                          |MKB_OUT1_PIN_Pin|MKB_OUT2_PIN_Pin|MKB_OUT3_PIN_Pin|MKB_OUT4_PIN_Pin
+  HAL_GPIO_WritePin(GPIOA, MKB_OUT1_PIN_Pin|MKB_OUT2_PIN_Pin|MKB_OUT3_PIN_Pin|MKB_OUT4_PIN_Pin
+                          |MKB_IN1_PIN_Pin|MKB_IN2_PIN_Pin|MKB_IN3_PIN_Pin|MKB_IN4_PIN_Pin
                           |BLED_PIN_Pin|RLED_PIN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, BUZZER1_OUT_PIN_Pin|LOAD_ACT_PIN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LOAD_ACT_PIN_GPIO_Port, LOAD_ACT_PIN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : LED_BOARD_Pin */
   GPIO_InitStruct.Pin = LED_BOARD_Pin;
@@ -68,23 +68,23 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LED_BOARD_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : MKB_IN1_PIN_Pin MKB_IN2_PIN_Pin MKB_IN3_PIN_Pin MKB_IN4_PIN_Pin
-                           MKB_OUT1_PIN_Pin MKB_OUT2_PIN_Pin MKB_OUT3_PIN_Pin MKB_OUT4_PIN_Pin
+  /*Configure GPIO pins : MKB_OUT1_PIN_Pin MKB_OUT2_PIN_Pin MKB_OUT3_PIN_Pin MKB_OUT4_PIN_Pin
+                           MKB_IN1_PIN_Pin MKB_IN2_PIN_Pin MKB_IN3_PIN_Pin MKB_IN4_PIN_Pin
                            BLED_PIN_Pin RLED_PIN_Pin */
-  GPIO_InitStruct.Pin = MKB_IN1_PIN_Pin|MKB_IN2_PIN_Pin|MKB_IN3_PIN_Pin|MKB_IN4_PIN_Pin
-                          |MKB_OUT1_PIN_Pin|MKB_OUT2_PIN_Pin|MKB_OUT3_PIN_Pin|MKB_OUT4_PIN_Pin
+  GPIO_InitStruct.Pin = MKB_OUT1_PIN_Pin|MKB_OUT2_PIN_Pin|MKB_OUT3_PIN_Pin|MKB_OUT4_PIN_Pin
+                          |MKB_IN1_PIN_Pin|MKB_IN2_PIN_Pin|MKB_IN3_PIN_Pin|MKB_IN4_PIN_Pin
                           |BLED_PIN_Pin|RLED_PIN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : BUZZER1_OUT_PIN_Pin LOAD_ACT_PIN_Pin */
-  GPIO_InitStruct.Pin = BUZZER1_OUT_PIN_Pin|LOAD_ACT_PIN_Pin;
+  /*Configure GPIO pin : LOAD_ACT_PIN_Pin */
+  GPIO_InitStruct.Pin = LOAD_ACT_PIN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(LOAD_ACT_PIN_GPIO_Port, &GPIO_InitStruct);
 
 }
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,12 +19,12 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "i2c.h"
-#include "usart.h"
+#include "tim.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "PCF8574_Driver.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -89,7 +89,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
-  MX_USART1_UART_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
