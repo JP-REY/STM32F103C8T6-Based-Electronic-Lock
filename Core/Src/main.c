434299469f000c8f1f32204020ba79673fc8f48c
Joao Pedro Rey <jprey.380@hotmail.com>
2026-07-23 11:56:56 -0300

    chore(cubemx): update .ioc project file
    - Added mx generated code configuration for timers TIM2,TIM3 and TIM4
    
    Updated board pins labels according to project specifications

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -89,8 +89,8 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
-  MX_TIM3_Init();
   MX_TIM2_Init();
+  MX_TIM3_Init();
   MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
 
