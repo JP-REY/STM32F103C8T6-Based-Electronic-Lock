63f98d522f09702855541a7824330fb4be537502
Joao Pedro Rey <jprey.380@hotmail.com>
2026-07-30 09:42:31 -0300

    chore(cubemx): update CubeMX configuration
    - Configure keypad row GPIOs as EXTI inputs to support interrupt-based key detection and future wake-up capability
    - Change I2C peripheral timing to Fast Mode (400 kHz)
    - Rename multiple board GPIO pins to improve readability and hardware signal identification in the generated project

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -227,12 +227,12 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     /**TIM3 GPIO Configuration
     PB4     ------> TIM3_CH1
     */
-    GPIO_InitStruct.Pin = BUZZER_TIM3CH1_Pin;
+    GPIO_InitStruct.Pin = BUZZER_PWM_TIM3CH1_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
-    HAL_GPIO_Init(BUZZER_TIM3CH1_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(BUZZER_PWM_TIM3CH1_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN TIM3_MspPostInit 1 */
 
@@ -248,12 +248,12 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     /**TIM4 GPIO Configuration
     PB9     ------> TIM4_CH4
     */
-    GPIO_InitStruct.Pin = LCD_BRIGHTNESS_TIM4CH4_Pin;
+    GPIO_InitStruct.Pin = HD44780_BRIGHTNESS_PWM_TIM4CH4_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
-    HAL_GPIO_Init(LCD_BRIGHTNESS_TIM4CH4_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(HD44780_BRIGHTNESS_PWM_TIM4CH4_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN TIM4_MspPostInit 1 */
 
